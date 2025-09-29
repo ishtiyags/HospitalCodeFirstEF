9e0e1b476c5887c15738aa431153d51bdca4e7a8
ishtiyags <165825704+ishtiyags@users.noreply.github.com>
2025-09-29 17:39:27 +0200

    Add files via upload

---
+++ Models/Doctors.cs
@@ -0,0 +1,12 @@
+﻿namespace HospitalApp.Models
+{
+    public class Doctor
+    {
+        public int DoctorId { get; set; }      // Primary Key
+        public string Name { get; set; }
+        public string Specialty { get; set; }
+
+        // Navigation property (One doctor can have many patients)
+        public List<Patient> Patients { get; set; }
+    }
+}
---
+++ Models/Patient.cs
@@ -0,0 +1,13 @@
+﻿namespace HospitalApp.Models
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }     // Primary Key
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // Foreign key
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; }     // Navigation property
+    }
+}
